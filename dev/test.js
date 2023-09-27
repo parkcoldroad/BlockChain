2f8d71483be4986b36d6c7428fd602177aa14ada
parkcoldroad <coldstreet@naver.com>
2023-09-27 11:35:22 +0900

    test: 트랜잭션 생성 테스트 추가

--- dev/test.js
+++ dev/test.js
@@ -1,6 +1,14 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(2389, 'OIUOERJFDS', '90ADSFGHJK');
-bitcoin.createNewBlock(333, 'DQWDQWDQWD', 'SDD52DQW');
-bitcoin.createNewBlock(111, 'DAS05QASD', 'WEQWE2E');
-console.log(bitcoin);
+bitcoin.createNewBlock(789457, 'SDASDAWQWFGQAWF2001', '78ASDASDQWR56');
+
+bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+
+bitcoin.createNewBlock(548764, 'AKCASDAS1ADS59', 'WPLDASD1ASD1SAG2');
+
+bitcoin.createNewTransaction(50, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+bitcoin.createNewTransaction(200, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+bitcoin.createNewTransaction(300, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+
+bitcoin.createNewBlock(15115, 'WPLDASD1ASD1SAG2', 'ASFCASQW131WE');
+console.log(bitcoin.chain[2]);
