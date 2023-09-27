6763dce1ad06474ddbf69f569aa68cc0e89bfa0d
parkcoldroad <coldstreet@naver.com>
2023-09-27 13:21:43 +0900

    test: 해시블록 테스트 추가

--- dev/test.js
+++ dev/test.js
@@ -1,14 +1,27 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(789457, 'SDASDAWQWFGQAWF2001', '78ASDASDQWR56');
+const previousBlockHash = 'OINAISDASDASDASD';
+const currentBlockData = [
+    {
+        amount : 10,
+        sender : 'ALEXHT845SJ5TKCJ2',
+        recipient : 'JENN5BG5DF6HT8NG9'
+    }
+];
+const nonce = 100;
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+// bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
-bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
 
-bitcoin.createNewBlock(548764, 'AKCASDAS1ADS59', 'WPLDASD1ASD1SAG2');
+// bitcoin.createNewBlock(789457, 'SDASDAWQWFGQAWF2001', '78ASDASDQWR56');
 
-bitcoin.createNewTransaction(50, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(200, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(300, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
 
-bitcoin.createNewBlock(15115, 'WPLDASD1ASD1SAG2', 'ASFCASQW131WE');
-console.log(bitcoin.chain[2]);
+// bitcoin.createNewBlock(548764, 'AKCASDAS1ADS59', 'WPLDASD1ASD1SAG2');
+
+// bitcoin.createNewTransaction(50, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewTransaction(200, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewTransaction(300, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+
+// bitcoin.createNewBlock(15115, 'WPLDASD1ASD1SAG2', 'ASFCASQW131WE');
+// console.log(bitcoin.chain[2]);
