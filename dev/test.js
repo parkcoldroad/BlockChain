8f8b795ecf350453a4d3c92e29330a46a772aac4
parkcoldroad <coldstreet@naver.com>
2023-09-27 14:21:46 +0900

    test: 제네시스 블록 테스트

--- dev/test.js
+++ dev/test.js
@@ -1,24 +1,25 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
-const previousBlockHash = 'OINAISDASDASDASD';
-const currentBlockData = [
-    {
-        amount : 10,
-        sender : 'ALEXHT845SJ5TKCJ2',
-        recipient : 'JENN5BG5DF6HT8NG9z'
-    },
-    {
-        amount : 300,
-        sender : 'ASDASD1AS21DD',
-        recipient : 'QTQWRDASDASD'
-    },
-    {
-        amount : 800,
-        sender : 'ASDASD1QW56R1QW',
-        recipient : 'ASFGQEGQEGNASD'
-    }
-];
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 74406));
+console.log(bitcoin);
+// const previousBlockHash = 'OINAISDASDASDASD';
+// const currentBlockData = [
+//     {
+//         amount : 10,
+//         sender : 'ALEXHT845SJ5TKCJ2',
+//         recipient : 'JENN5BG5DF6HT8NG9z'
+//     },
+//     {
+//         amount : 300,
+//         sender : 'ASDASD1AS21DD',
+//         recipient : 'QTQWRDASDASD'
+//     },
+//     {
+//         amount : 800,
+//         sender : 'ASDASD1QW56R1QW',
+//         recipient : 'ASFGQEGQEGNASD'
+//     }
+// ];
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 74406));
 // console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
 // bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
